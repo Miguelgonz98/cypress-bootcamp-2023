48bb91704e45f6f0c0ea072863623c8f493b1761
Miguel González Carvajal <migonzalez@mobilize.net>
2023-05-19 11:00:24 -0600

    Development of Create Bank Account Test Case

--- cypress/e2e/migonzalez_assignment2.cy.js
+++ cypress/e2e/migonzalez_assignment2.cy.js
@@ -1,7 +1,25 @@
 describe('Bootcamp 2023 - Assignment 2', () => {
 
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() =>{
+    cy.visit('http://localhost:3000/signin')
+    cy.get('#username').clear().type('Katharina_Bernier')
+    cy.get('#password').clear().type('s3cret')
+    cy.get('.MuiButton-label').click()
+    cy.get('h6[data-test="sidenav-username"]').should('have.text', '@Katharina_Bernier')
+  })
+
+  it('Create New Bank Account', () => {
+    cy.get('span.MuiTypography-root').contains('Bank Accounts').click()
+    cy.get('#root div.MuiContainer-root.makeStyles-container-20.MuiContainer-maxWidthMd div:nth-child(2) > a').click()
+    cy.get('#bankaccount-bankName-input').clear().type('Test Account')
+    cy.get('#bankaccount-routingNumber-input').clear().type('123456789')
+    cy.get('#bankaccount-accountNumber-input').clear().type('123456789')
+    cy.get('button[data-test="bankaccount-submit"]').click()
+    cy.get('div.MuiContainer-root.makeStyles-container-20.MuiContainer-maxWidthMd div:nth-child(1) p').contains('Test Account').should('exist')
+  })
+
+  afterEach(() =>{
+    cy.get('span.MuiTypography-root').contains('Logout').click()
   })
 
 })
