147bfe8afdf52b9e43d8d765b16dbc548f8e5806
Miguel González Carvajal <migonzalez@mobilize.net>
2023-06-13 17:12:34 -0600

    Initial Development of Assignment 4

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    apiUrl: "http://localhost:3001"
+  },
   e2e: {
     baseUrl: "http://localhost:3000",
     watchForFileChanges: false,
---
+++ cypress/e2e/migonzalez_assignment4.cy.js
@@ -0,0 +1,35 @@
+import { LoginPage } from "../page-objects/pages/login";
+const apiBankAccounts = `${Cypress.env("apiUrl")}/bankAccounts`;
+
+describe('Real World App Bank Account API Tests', () => {
+  const UUID = '1' + Math.random().toString();
+  const bankAccountName = "Test Account " + UUID;
+  const routingNumber = '987654321';
+  const accountNumber = '123456789';
+  let bankAccountID;
+  
+    beforeEach(() => {
+      cy.visit('/');
+      LoginPage.login('Katharina_Bernier','s3cret');
+      cy.request("POST", `${apiBankAccounts}`, {
+        bankName: bankAccountName,
+        routingNumber: routingNumber,
+        accountNumber: accountNumber
+      }).then((response) => {
+        bankAccountID = response.body.account.id;
+        expect(response.status).to.eq(200);
+      });
+    });
+
+    it("should create a bank account via API", () => {
+      cy.visit(`/bankaccounts/${bankAccountID}`);
+    });
+
+    it.only("Should get all bank accounts", () => {
+      cy.request("GET", `${apiBankAccounts}`, {}).then((response) => {
+        expect(response.status).to.eq(200)
+      })
+    });
+
+    
+  });
