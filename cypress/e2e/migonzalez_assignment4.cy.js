bc92becb85f7e34f8097993537087fd8c2403a33
Miguel González Carvajal <migonzalez@mobilize.net>
2023-06-14 18:52:51 -0600

    Changes in Assignment 4

--- cypress/e2e/migonzalez_assignment4.cy.js
+++ cypress/e2e/migonzalez_assignment4.cy.js
@@ -1,4 +1,5 @@
 import { LoginPage } from "../page-objects/pages/login";
+import { BankAccountsPage } from "../page-objects/pages/bankAccounts";
 const apiBankAccounts = `${Cypress.env("apiUrl")}/bankAccounts`;
 
 describe('Real World App Bank Account API Tests', () => {
@@ -6,38 +7,56 @@ describe('Real World App Bank Account API Tests', () => {
   const bankAccountName = "Test Account " + UUID;
   const routingNumber = '987654321';
   const accountNumber = '123456789';
-  let bankAccountID;
+  let bankAccountsID = [];
+  let bankAccountsName = [];
   
     beforeEach(() => {
       cy.visit('/');
       LoginPage.login('Katharina_Bernier','s3cret');
-      cy.request("POST", `${apiBankAccounts}`, {
-        bankName: bankAccountName,
-        routingNumber: routingNumber,
-        accountNumber: accountNumber
-      }).then((response) => {
-        bankAccountID = response.body.account.id;
-        expect(response.status).to.eq(200);
+    });
+
+    afterEach(() =>{
+      cy.get('span.MuiTypography-root').contains('Logout').click();
+    });
+
+    it("Should create a 5 bank accounts via API", () => {
+      for (let i = 0; i < 5; i++) {
+        cy.request("POST", `${apiBankAccounts}`, {
+          bankName: bankAccountName+i,
+          routingNumber: routingNumber+i,
+          accountNumber: accountNumber+i
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+          bankAccountsID.push(response.body.account.id);
+          bankAccountsName.push(bankAccountName+i);
+        });
+      }
+      cy.visit("/bankAccounts");
+      bankAccountsName.forEach(e => {
+        BankAccountsPage.elements.getBankAccounts().contains(e).should('exist');
       });
     });
 
-    it("should create a bank account via API", () => {
-      cy.visit(`/bankaccounts/${bankAccountID}`);
-    });
-
-    it.only("Should get all bank accounts", () => {
-      cy.request("GET", `${apiBankAccounts}`, {}).then((response) => {
-        expect(response.status).to.eq(200)
+    it("Should get all bank accounts", () => {
+      bankAccountsID.forEach(e => {
+        cy.request("GET", `${apiBankAccounts}/${e}`, {
+        }).then((response) => {
+          expect(response.status).to.eq(200)
+        })
       })
     });
 
-    it.only("Should delete a transaction", () => {
-      cy.request("DELETE", `${apiBankAccounts}/${bankAccountID}`, {
-        isDeleted: true
-      }).then((response) => {
-        expect(response.status).to.eq(200)
+    it("Should delete 5 last bank accounts", () => {
+      bankAccountsID.forEach(e => {
+        cy.request("DELETE", `${apiBankAccounts}/${e}`, {
+          isDeleted: true
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+        })
       })
+      cy.visit("/bankAccounts");
+      bankAccountsName.forEach(e => {
+        BankAccountsPage.elements.getBankAccounts().contains(`${e} (Deleted)`).should('exist');
+      });
     });
-
-    
   });
